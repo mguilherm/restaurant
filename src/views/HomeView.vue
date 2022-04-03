c579ae164eb29f3f40e269889b04256d32f3f05a
Marcos Guilherme Silva <marcosguilherme.silva@outlook.com>
2022-04-03 16:48:47 -0300

    json serve and axios install

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,11 +7,17 @@
 <script>
 
 import CategoryMenu from '@/components/CategoryMenu.vue'
+import axios from 'axios'
 
 export default {
   name: 'HomeView',
   components: {
     CategoryMenu
+  },
+  created(){
+    axios.get('http://localhost:3000/burguers').then( response => {
+      console.log(response)
+    })
   }
 }
 </script>
